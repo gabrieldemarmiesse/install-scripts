6dd4f225132de2b5121523dd03bc2adc45e3605e
Gabriel de Marmiesse <gabrieldemarmiesse@gmail.com>
2020-08-13 10:21:11 +0200

    Create install-nvidia-docker.sh

---
+++ install-nvidia-docker.sh
@@ -0,0 +1,33 @@
+set -x -e
+
+apt-get update
+apt-get install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg-agent \
+    software-properties-common
+    
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
+
+apt-key fingerprint 0EBFCD88
+
+add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+
+apt-get update
+apt-get install docker-ce docker-ce-cli containerd.io
+docker run hello-world
+
+
+distribution=$(. /etc/os-release;echo $ID$VERSION_ID)
+curl -s -L https://nvidia.github.io/nvidia-docker/gpgkey | apt-key add -
+curl -s -L https://nvidia.github.io/nvidia-docker/$distribution/nvidia-docker.list | tee /etc/apt/sources.list.d/nvidia-docker.list
+
+apt-get update && sudo apt-get install -y nvidia-container-toolkit
+systemctl restart docker
+
+sleep 5
+docker run --gpus all nvidia/cuda:10.0-base nvidia-smi
